f6f84ee59208b93f400ca74852b50a7995f33cdc
Justin van Ziel <justin.van.ziel@gmail.com>
2020-03-24 19:13:58 +0100

    Switch cases toegevoegd om 'uit-gecommente code' tegen te gaan, op verzoek van Florian

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -1,5 +1,4 @@
 #include "StudentPreProcessing.h"
-#include "IntensityImageStudent.h"
 #include "PixelType.h"
 #include "ImageFactory.h"
 #include <algorithm>
@@ -12,57 +11,58 @@ IntensityImage* StudentPreProcessing::stepToIntensityImage(const RGBImage& image
     // FHU: Ook je return type geeft aan dat je een IntensityImage teruggeeft maar je geeft een IntensityImageStudent terug.
 	// JUS: DONE, gebruik gemaakt van de ImageFactory:: functies.
 	// JUS: Ook returned het nu een IntensityImage i.p.v. een IntensityImageStudent.
+	// JUS: En als laatst heb ik Switch cases toegevoegd om zo 'uit-gecommente' code tegen te gaan.
 	IntensityImage* newIntensityImage = ImageFactory::newIntensityImage(image.getWidth(), image.getHeight());
 
 	int loopCount = image.getWidth() * image.getHeight();
 
-	for (int i = 0; i < loopCount; i++)
-	{
-		auto oldRGB = image.getPixel(i);
-		//create a list containing the old values
-		std::initializer_list<unsigned char> tempVec = { oldRGB.r,oldRGB.g,oldRGB.b };
-		//this is the actual algorithm (taking the max, that's it)
-		Intensity newIntensityPixel = (std::max(tempVec));
-		newIntensityImage->setPixel(i, newIntensityPixel);
+	enum class methode { INTENSITY, VALUE, LUMINANCE };
+	methode currentMethode = methode::LUMINANCE;
+
+	switch (currentMethode) {
+
+		case methode::INTENSITY:
+
+			for (int i = 0; i < loopCount; i++){
+				auto oldRGB = image.getPixel(i);
+				Intensity newIntensityPixel = (oldRGB.r + oldRGB.g + oldRGB.b) / 3;
+				newIntensityImage->setPixel(i, newIntensityPixel);
+			}
+
+			break;
+
+		case methode::VALUE:
+
+			for (int i = 0; i < loopCount; i++){
+				auto oldRGB = image.getPixel(i);
+				std::initializer_list<unsigned char> tempVec = { oldRGB.r,oldRGB.g,oldRGB.b };
+				Intensity newIntensityPixel = (std::max(tempVec));
+				newIntensityImage->setPixel(i, newIntensityPixel);
+			}
+
+			break;
+
+		case methode::LUMINANCE:
+			
+			for (int i = 0; i < loopCount; i++){
+				auto oldRGB = image.getPixel(i);
+				Intensity newIntensityPixel = ((0.3f *oldRGB.r) + (0.59f*oldRGB.g) + (0.11f*oldRGB.b));
+				newIntensityImage->setPixel(i, newIntensityPixel);
+			}
+
+			break;
+
+		default:
+			break;
+
 	}
+
+
 
 	return newIntensityImage;
 
 }
 
-//luminance methode code for converting a colored image to a gray image
-//IntensityImage* StudentPreProcessing::stepToIntensityImage(const RGBImage& image) const {
-//	IntensityImage* newIntensityImage = ImageFactory::ImplementationStudent::newIntensityImage(image.getWidth(), image.getHeight());
-//
-//	int loopCount = image.getWidth() * image.getHeight();
-//
-//	for (int i = 0; i < loopCount; i++)
-//	{
-//		auto oldRGB = image.getPixel(i);
-//		//this is the actual algorithm
-//		Intensity newIntensityPixel = ((0.3f *oldRGB.r) + (0.59f*oldRGB.g) + (0.11f*oldRGB.b));
-//		newIntensityImage->setPixel(i, newIntensityPixel);
-//	}
-//
-//	return newIntensityImage;
-//}
-
-//intensity methode code for converting a colored image to a gray image
-//IntensityImage * StudentPreProcessing::stepToIntensityImage(const RGBImage &image) const {
-//	IntensityImage* newIntensityImage = ImageFactory::ImplementationStudent::newIntensityImage(image.getWidth(), image.getHeight());
-//
-//	int loopCount = image.getWidth() * image.getHeight();
-//
-//	for (int i = 0; i < loopCount; i++)
-//	{
-//		auto oldRGB = image.getPixel(i);
-//		//this is the actual algorithm
-//		Intensity newIntensityPixel = (oldRGB.r + oldRGB.g + oldRGB.b) / 3;
-//		newIntensityImage->setPixel(i, newIntensityPixel);
-//	}
-//
-//	return newIntensityImage;
-//}
 
 IntensityImage * StudentPreProcessing::stepScaleImage(const IntensityImage &image) const {
 	return nullptr;
